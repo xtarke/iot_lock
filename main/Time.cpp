9d8e4e7d4ddea349dce7a19572e8cd6df88b6e98
Renan Augusto Starke <xtarke@gmail.com>
2022-08-16 17:26:47 -0300

    Added Time interface.

---
+++ main/Time.cpp
@@ -0,0 +1,24 @@
+/*
+ * Time.cpp
+ *
+ *  Created on: Aug 16, 2022
+ *      Author: Renan Augusto Starke
+ */
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "Time.h"
+
+Time::Time() {
+	// TODO Auto-generated constructor stub
+
+}
+
+
+uint32_t Time::GetTime(){
+
+	TickType_t ticks = xTaskGetTickCount();
+
+	return (uint32_t)ticks ;
+}
---
+++ main/Time.h
@@ -0,0 +1,21 @@
+/*
+ * Time.h
+ *
+ *  Created on: Aug 16, 2022
+ *      Author: Renan Augusto Starke
+ */
+
+#ifndef MAIN_TIME_H_
+#define MAIN_TIME_H_
+
+#include <stdint.h>
+
+class Time {
+public:
+	Time();
+
+	uint32_t GetTime();
+
+};
+
+#endif /* MAIN_TIME_H_ */
